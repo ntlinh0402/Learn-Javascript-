4ee65d19978175e49dd4cfba88a458df4998e5c2
Thuy Linh <84694214+ntlinh0402@users.noreply.github.com>
2021-12-28 16:01:07 +0700

    update js

--- Call back/main.js
+++ Call back/main.js
@@ -1,17 +1,29 @@
+Array.prototype.map2 =function(callback){
+    var arrayLength = this.length;
+    for (var i=0; i < arrayLength; ++i){
+        callback(this[i], i);
+    }
+}
 var courses =[
     'js',
     'php',
     'ruby'
 ];
+var htmls = courses.map2(function(course){
+    // console.log(index,course)
+    return `<h2>${course}</h2>`
+
+});
 // courses.map(function(course){
 //     console.log(course);
 //     return `<h2>${course}</h2>`;
 
 // });
 // console.log(htmls.join(''))
-var htmls = courses.map2(function(course){
-    // console.log(course);
-    return `<h2>${course}</h2>`;
 
-});
-console.log(htmls.join(''))
+// var htmls = courses.map2(function(course){
+//     // console.log(course);
+//     return `<h2>${course}</h2>`;
+
+// });
+// console.log(htmls.join(''))
