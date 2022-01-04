8e1b2abe0a7363f6d1f9c91301d9712cce7f0839
Thuy Linh <84694214+ntlinh0402@users.noreply.github.com>
2022-01-04 17:20:36 +0700

    updatw preventDefault

---
+++ DOM/preventDefault StopPropagation/main.js
@@ -0,0 +1,24 @@
+// var aElements =document.links;
+// console.log(aElements);
+// for(var i=0; i<aElements.length; ++i){
+//     aElements[i].onclick =function(e){
+//         if(!e.target.href.startsWith('https://fullstack.edu.vn/')){
+//             e.preventDefault();
+//         }
+//         console.log(e.target.href);
+//     }
+// }
+
+// ex2
+
+
+// var ulElements =document.querySelector('ul');
+// ulElements.onmousedown=function(e){
+//     e.preventDefault();
+// }
+// ulElements.onclick=function(e){
+//     console.log(e.target)
+// }
+
+
+// STOP
